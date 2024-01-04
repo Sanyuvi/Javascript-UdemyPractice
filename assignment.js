327406618059d924da71a327e4c93ee101dd7150
Sanyuvi <santhoshh2197@gmail.com>
2024-01-04 23:44:02 +0530

    day 2 learning

---
+++ assignment.js
@@ -0,0 +1,4 @@
+let country = "India";
+let continent = "Asia";
+let population = "14286millions";
+console.log(country, continent, population);
--- script.js
+++ script.js
@@ -1,5 +1,28 @@
-let js = "amazing";
-if (js === "amazing") alert("JS is FUN");
+// let js = "amazing";
+// if (js === "amazing") alert("JS is FUN");
 
-let results = 45 + 34 - 28;
-console.log(results);
+// let results = 45 + 34 - 28;
+// console.log(results);
+
+// typeof
+
+const a = 234;
+console.log(typeof a);
+
+// operators
+
+const now = 2037;
+const mikeAge = now - 2008;
+console.log(mikeAge * 2, mikeAge / 3, 3 ** 2);
+// 3**2 means 3 to the power of 2 = 3 * 3
+
+//concatination
+
+const firstName = "Virat";
+const lastName = "Kohli";
+
+console.log(firstName + " " + lastName);
+
+let x = 10 + 5;
+x += 10;
+// x+=10 means X=X+10
