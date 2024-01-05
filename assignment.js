aabc96a98c7b2fe19ec36bd11efb5a1c1a5af409
Sanyuvi <santhoshh2197@gmail.com>
2024-01-05 23:36:45 +0530

    day 3 learning

--- assignment.js
+++ assignment.js
@@ -2,3 +2,24 @@ let country = "India";
 let continent = "Asia";
 let population = "14286millions";
 console.log(country, continent, population);
+//practice 1
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn);
+
+if (BMIMark > BMIJohn) {
+  console.log(`Mark's BMI is higher than John's!`);
+} else {
+  console.log(`John's BMI is higher than Mark's!`);
+}
+
+if (BMIMark > BMIJohn) {
+  console.log(`Mark's BMI ${BMIMark} is higher than John's ${BMIJohn}!`);
+} else {
+  console.log(`John's BMI ${BMIJohn} is higher than Mark's BMI ${BMIMark}!`);
+}
--- script.js
+++ script.js
@@ -26,3 +26,63 @@ console.log(firstName + " " + lastName);
 let x = 10 + 5;
 x += 10;
 // x+=10 means X=X+10
+
+//Template literals
+
+const fullName = "Mike";
+const job = "dancer";
+const year = 2025;
+
+console.log(
+  "I'm " + fullName + " a " + job + " of " + (year - 2000) + " years old"
+);
+console.log(`I'm ${fullName} a ${job} of ${year - 2000} years old`);
+
+//If else:
+
+const age = 22;
+const isEligible = age >= 18;
+
+if (isEligible) {
+  console.log(`He can start driving 🚗;`);
+} else {
+  console.log(`Not eligible to drive `);
+}
+//Truthy falsy values
+// falsy value: 0, " ", undefined, null, Nan
+
+const money = 0;
+if (money) {
+  console.log(`spend it all!`);
+} else {
+  console.log(`Don't spend it!`);
+}
+
+let height;
+if (height) {
+  console.log(`Height is defined`);
+} else {
+  console.log(`Height is not defined`);
+}
+
+//Boolean logics(AND, OR & NOT)
+
+const hasDriversLicense = true; //A
+const hasGoodVision = false; //B
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+
+if (hasDriversLicense && hasGoodVision) {
+  console.log(`He is able to drive`);
+} else {
+  console.log(`Someone else should drive...`);
+}
+
+const isTiried = true;
+if (hasDriversLicense && hasGoodVision && !isTiried) {
+  console.log(`He is able to drive`);
+} else {
+  console.log(`Someone else should drive...`);
+}
