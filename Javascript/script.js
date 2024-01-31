0b397be48c90b4dcacaf822cf020eb620824ba2c
Sanyuvi <santhoshh2197@gmail.com>
2024-01-31 23:45:55 +0530

    more concepts added

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,3 +1,4 @@
+"use strict";
 // let js = "amazing";
 // if (js === "amazing") alert("JS is FUN");
 
@@ -188,3 +189,139 @@
 //     console.log(`Exercise ${exercise}:Lifting weights reptition ${rep}`);
 //   }
 // }
+
+// const flights =
+//   "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
+
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+// const getCode = (str) => str.slice(0, 3).toUpperCase();
+
+// for (const flight of flights.split("+")) {
+//   const [type, from, to, time] = flight.split(";");
+//   const output = `${type.startsWith("_Delayed") ? "🔴" : ""}${type.replaceAll(
+//     "_",
+//     " "
+//   )} ${getCode(from)} ${getCode(to)} (${time.replace(":", "h")})`.padStart(36);
+//   console.log(output);
+// }
+
+// const flight = "LH340";
+// const san = {
+//   name: "Santhosh",
+//   passport: 7788446657,
+// };
+// const checkIn = function (flightNum, passenger) {
+//   flightNum = "LV778";
+//   passenger.name = "Mr." + passenger.name;
+//   if (passenger.passport === san.passport) {
+//     alert("CheckedIn");
+//   } else {
+//     alert("Wrong entry");
+//   }
+// };
+// checkIn(flight, san);
+// console.log(flight);
+// console.log(san);
+
+// Functions using Callback fun
+
+const oneWord = function (str) {
+  return str.replace(/ /g, " ").toLowerCase();
+};
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(" ");
+  return [first.toUpperCase(), ...others].join(" ");
+};
+//Higher order fun
+const transformer = function (str, fn) {
+  console.log(`Origunal string :${str}`);
+  console.log(`Transformed string :${fn(str)}`);
+  console.log(`Transformed by :${fn.name}`);
+};
+transformer("javascript is best lang", upperFirstWord);
+transformer("javascript is best lang", oneWord);
+
+//function return functions
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+const greetingHey = greet("Hey");
+greetingHey("Jonas");
+
+const greetArr = (greeting) => (name) => console.log(`${greeting} ${name}`);
+
+// Call & Apply method
+
+const spiceJet = {
+  airline: "spicejet",
+  code: "SP998",
+  bookings: [],
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.code} ${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.code} ${flightNum}`, name });
+  },
+};
+
+spiceJet.book(234, "Santhosh");
+spiceJet.book(445, "janani");
+
+const spiceWings = {
+  airline: "Flywing",
+  code: "hF998",
+  bookings: [],
+};
+const book = spiceJet.book;
+book.call(spiceJet, 345, "Deena");
+book.call(spiceWings, 787, "Bai");
+
+//apply method
+const flightbox = [346, "Mandela"];
+book.apply(spiceWings, flightbox);
+//bind method
+
+const bookSW = book.bind(spiceJet);
+bookSW(45, "Father");
+
+const bookFD = book.bind(spiceWings, 46);
+bookFD("manoj");
+
+const addVAT = function (rate) {
+  return function (value) {
+    console.log(`Result:`, value + value * rate);
+  };
+};
+const addTax = addVAT(0.23);
+addTax(100);
+
+//Closure
+
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 555;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+
+// Reassigning f function
+
+h();
+f();
