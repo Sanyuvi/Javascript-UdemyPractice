e5c4911a516354d808c42c181a6097d070988534
Sanyuvi <santhoshh2197@gmail.com>
2024-02-04 13:26:12 +0530

    more code challenges were added

--- Javascript/assignment.js
+++ Javascript/assignment.js
@@ -1,3 +1,4 @@
+"use strict";
 // let country = "India";
 // let continent = "Asia";
 // let population = "14286millions";
@@ -152,29 +153,201 @@
 
 // ans code:
 
-const calcTip = function (bill) {
-  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// const calcTip = function (bill) {
+//   return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// };
+
+// /* Write your code below. Good luck! 🙂 */
+
+// const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+// const tips = [];
+// const totals = [];
+// for (let i = 0; i < bills.length; i++) {
+//   const tip = calcTip(bills[i]);
+//   tips.push(tip);
+//   totals.push(tip + bills[i]);
+// }
+// console.log(bills, tips, totals);
+
+// const calcAvg = function (arr) {
+//   let sum = 0;
+//   for (i = 0; i < arr.length; i++) {
+//     sum += arr[i];
+//   }
+//   return sum / arr.length;
+// };
+// calcAvg(totals);
+
+// console.log(calcAvg(totals));
+
+// Coding Challenge #1
+
+// We're building a football betting app (soccer for my American friends 😅)!
+
+// Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
+
+// 1. Create one player array for each team (variables 'players1' and 'players2')
+// 2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+// 3. Create an array 'allPlayers' containing all players of both teams (22 players)
+// 4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+// 5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+// 6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+// 7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+
+// // TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+
+const game = {
+  team1: "Bayern Munich",
+  team2: "Borrussia Dortmund",
+  players: [
+    [
+      "Neuer",
+      "Pavard",
+      "Martinez",
+      "Alaba",
+      "Davies",
+      "Kimmich",
+      "Goretzka",
+      "Coman",
+      "Muller",
+      "Gnarby",
+      "Lewandowski",
+    ],
+    [
+      "Burki",
+      "Schulz",
+      "Hummels",
+      "Akanji",
+      "Hakimi",
+      "Weigl",
+      "Witsel",
+      "Hazard",
+      "Brandt",
+      "Sancho",
+      "Gotze",
+    ],
+  ],
+  score: "4:0",
+  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+  date: "Nov 9th, 2037",
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
 };
 
-/* Write your code below. Good luck! 🙂 */
+// // Sol:
+// const [players1, players2] = game.players;
+// console.log(players1, players2);
 
-const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
-const tips = [];
-const totals = [];
-for (let i = 0; i < bills.length; i++) {
-  const tip = calcTip(bills[i]);
-  tips.push(tip);
-  totals.push(tip + bills[i]);
+// const [gk, ...fieldPlayers] = players1;
+// console.log(gk, fieldPlayers);
+
+// const allPlayers = [...players1, ...players2];
+// console.log(allPlayers);
+
+// const players1Final = [...players1, "Thiago", "Coutinho", "Perisic"];
+// console.log(players1Final);
+
+// const {
+//   odds: { team1, x: draw, team2 },
+// } = game;
+// console.log(team1, draw, team2);
+
+// const printGoals = function (...players) {
+//   console.log(players);
+//   console.log(`${players.length} goals were scored`);
+// };
+// printGoals(...game.scored);
+
+// team1 > team2 && console.log(`Team1 is morelikely to win`);
+// team1 < team2 && console.log(`Team1 is morelikely to win`);
+
+// Coding Challenge #2
+
+// Let's continue with our football betting app!
+
+// 1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
+// 2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
+// 3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
+//       Odd of victory Bayern Munich: 1.33
+//       Odd of draw: 3.25
+//       Odd of victory Borrussia Dortmund: 6.5
+// Get the team names directly from the game object, don't hardcode them (except for "draw"). HINT: Note how the odds and the game objects have the same property names 😉
+
+// BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
+//       {
+//         Gnarby: 1,
+//         Hummels: 1,
+//         Lewandowski: 2
+//       }
+
+// Sol:
+// const displayScorers = [...game.scored];
+// console.log(displayScorers);
+// for (const [i] of displayScorers.entries()) {
+//   console.log(`Goal ${i + 1}: ${displayScorers[i]}`);
+// }
+
+// let average = 0;
+// const odds = Object.values(game.odds);
+// for (const odd of odds) {
+//   average += odd;
+// }
+// average /= odds.length;
+// console.log(average);
+
+// for (const [team, odd] of Object.entries(game.odds)) {
+//   const teamstr = team === "x" ? "draw" : `victory ${game[team]}`;
+//   console.log(`Odd of ${teamstr} ${odd}`);
+// }
+
+// game.scorers = {
+//   Gnarby: 1,
+//   Hummels: 1,
+//   Lewandowski: 2,
+// };
+// console.log(game);
+
+///////////////////////////////////////
+// Coding Challenge #3
+
+/* 
+Let's continue with our football betting app! This time, we have a map with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
+
+1. Create an array 'events' of the different game events that happened (no duplicates)
+2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+      [FIRST HALF] 17: ⚽️ GOAL
+
+GOOD LUCK 😀
+*/
+
+const gameEvents = new Map([
+  [17, "⚽️ GOAL"],
+  [36, "🔁 Substitution"],
+  [47, "⚽️ GOAL"],
+  [61, "🔁 Substitution"],
+  [64, "🔶 Yellow card"],
+  [69, "🔴 Red card"],
+  [70, "🔁 Substitution"],
+  [72, "🔁 Substitution"],
+  [76, "⚽️ GOAL"],
+  [80, "⚽️ GOAL"],
+  [92, "🔶 Yellow card"],
+]);
+
+const events = [...new Set(gameEvents.values())];
+console.log(gameEvents);
+
+gameEvents.delete(64);
+console.log(
+  `An event happened, on average, every ${90 / gameEvents.size} minutes`
+);
+
+for (const [t, e] of gameEvents) {
+  t <= 45 && console.log(`[First Half] ${t}:${e}`);
+  t >= 45 && console.log(`[Second Half] ${t}:${e}`);
 }
-console.log(bills, tips, totals);
-
-const calcAvg = function (arr) {
-  let sum = 0;
-  for (i = 0; i < arr.length; i++) {
-    sum += arr[i];
-  }
-  return sum / arr.length;
-};
-calcAvg(totals);
-
-console.log(calcAvg(totals));
--- Javascript/script.js
+++ Javascript/script.js
@@ -1,327 +1,327 @@
-"use strict";
-// let js = "amazing";
-// if (js === "amazing") alert("JS is FUN");
+// "use strict";
+// // let js = "amazing";
+// // if (js === "amazing") alert("JS is FUN");
 
-// let results = 45 + 34 - 28;
-// console.log(results);
+// // let results = 45 + 34 - 28;
+// // console.log(results);
 
-// typeof
+// // typeof
 
-// const a = 234;
-// console.log(typeof a);
+// // const a = 234;
+// // console.log(typeof a);
 
-// // operators
+// // // operators
 
-// const now = 2037;
-// const mikeAge = now - 2008;
-// console.log(mikeAge * 2, mikeAge / 3, 3 ** 2);
-// // 3**2 means 3 to the power of 2 = 3 * 3
+// // const now = 2037;
+// // const mikeAge = now - 2008;
+// // console.log(mikeAge * 2, mikeAge / 3, 3 ** 2);
+// // // 3**2 means 3 to the power of 2 = 3 * 3
 
-// //concatination
+// // //concatination
 
-// const firstName = "Virat";
-// const lastName = "Kohli";
+// // const firstName = "Virat";
+// // const lastName = "Kohli";
 
-// console.log(firstName + " " + lastName);
+// // console.log(firstName + " " + lastName);
 
-// let x = 10 + 5;
-// x += 10;
-// // x+=10 means X=X+10
+// // let x = 10 + 5;
+// // x += 10;
+// // // x+=10 means X=X+10
 
-// //Template literals
+// // //Template literals
 
-// const fullName = "Mike";
-// const job = "dancer";
-// const year = 2025;
+// // const fullName = "Mike";
+// // const job = "dancer";
+// // const year = 2025;
 
-// console.log(
-//   "I'm " + fullName + " a " + job + " of " + (year - 2000) + " years old"
-// );
-// console.log(`I'm ${fullName} a ${job} of ${year - 2000} years old`);
+// // console.log(
+// //   "I'm " + fullName + " a " + job + " of " + (year - 2000) + " years old"
+// // );
+// // console.log(`I'm ${fullName} a ${job} of ${year - 2000} years old`);
 
-// //If else:
+// // //If else:
 
-// const age = 22;
-// const isEligible = age >= 18;
+// // const age = 22;
+// // const isEligible = age >= 18;
 
-// if (isEligible) {
-//   console.log(`He can start driving 🚗;`);
-// } else {
-//   console.log(`Not eligible to drive `);
-// }
-// //Truthy falsy values
-// // falsy value: 0, " ", undefined, null, Nan
+// // if (isEligible) {
+// //   console.log(`He can start driving 🚗;`);
+// // } else {
+// //   console.log(`Not eligible to drive `);
+// // }
+// // //Truthy falsy values
+// // // falsy value: 0, " ", undefined, null, Nan
 
-// const money = 0;
-// if (money) {
-//   console.log(`spend it all!`);
-// } else {
-//   console.log(`Don't spend it!`);
-// }
+// // const money = 0;
+// // if (money) {
+// //   console.log(`spend it all!`);
+// // } else {
+// //   console.log(`Don't spend it!`);
+// // }
 
-// let height;
-// if (height) {
-//   console.log(`Height is defined`);
-// } else {
-//   console.log(`Height is not defined`);
-// }
+// // let height;
+// // if (height) {
+// //   console.log(`Height is defined`);
+// // } else {
+// //   console.log(`Height is not defined`);
+// // }
 
-// //Boolean logics(AND, OR & NOT)
+// // //Boolean logics(AND, OR & NOT)
 
-// const hasDriversLicense = true; //A
-// const hasGoodVision = false; //B
+// // const hasDriversLicense = true; //A
+// // const hasGoodVision = false; //B
 
-// console.log(hasDriversLicense && hasGoodVision);
-// console.log(hasDriversLicense || hasGoodVision);
-// console.log(!hasDriversLicense);
+// // console.log(hasDriversLicense && hasGoodVision);
+// // console.log(hasDriversLicense || hasGoodVision);
+// // console.log(!hasDriversLicense);
 
-// if (hasDriversLicense && hasGoodVision) {
-//   console.log(`He is able to drive`);
-// } else {
-//   console.log(`Someone else should drive...`);
-// }
+// // if (hasDriversLicense && hasGoodVision) {
+// //   console.log(`He is able to drive`);
+// // } else {
+// //   console.log(`Someone else should drive...`);
+// // }
 
-// const isTiried = true;
-// if (hasDriversLicense && hasGoodVision && !isTiried) {
-//   console.log(`He is able to drive`);
-// } else {
-//   console.log(`Someone else should drive...`);
-// }
+// // const isTiried = true;
+// // if (hasDriversLicense && hasGoodVision && !isTiried) {
+// //   console.log(`He is able to drive`);
+// // } else {
+// //   console.log(`Someone else should drive...`);
+// // }
 
-// // Function declaration
+// // // Function declaration
 
-// function freshJuice(apple, orange) {
-//   const juice = `juice with ${apple} apple and ${orange} orange`;
-//   return juice;
-// }
-// const drinkJuice = freshJuice(4, 3);
-// console.log(drinkJuice);
+// // function freshJuice(apple, orange) {
+// //   const juice = `juice with ${apple} apple and ${orange} orange`;
+// //   return juice;
+// // }
+// // const drinkJuice = freshJuice(4, 3);
+// // console.log(drinkJuice);
 
-// function calAge1(birthYear) {
-//   return 2037 - birthYear;
-// }
-// const age1 = calAge1(2012);
-// console.log(age1);
+// // function calAge1(birthYear) {
+// //   return 2037 - birthYear;
+// // }
+// // const age1 = calAge1(2012);
+// // console.log(age1);
 
-// // function expression
-// const calAge2 = function (birthYear) {
-//   return 2037 - birthYear;
+// // // function expression
+// // const calAge2 = function (birthYear) {
+// //   return 2037 - birthYear;
+// // };
+// // const age2 = calAge2(2008);
+// // console.log(age2);
+
+// // Arrow function
+
+// // const ageUntilRetierment = (birthYear, bestName) => {
+// //   const age = 2037 - birthYear;
+// //   const retiredAge = 60 - age;
+// //   return `${bestName} retires at the age of ${retiredAge}`;
+// // };
+// // console.log(ageUntilRetierment(2004, "Anil"));
+
+// // function calling another function
+// // function cutPeices(fruit) {
+// //   return fruit * 5;
+// // }
+
+// // function freshJuice(apple, orange) {
+// //   const appleJuice = cutPeices(apple);
+// //   const orangeJuice = cutPeices(orange);
+// //   const juice = `juice with ${appleJuice} apple and ${orangeJuice} orange`;
+// //   return juice;
+// // }
+// // const drinkJuices = freshJuice(4, 3);
+// // console.log(drinkJuice);
+// // Arrays exercise
+
+// // const calcAge = function (birthYear) {
+// //   return 2037 - birthYear;
+// // };
+
+// // const years = [2008, 2015, 1998, 2000];
+
+// // console.log(calcAge(years[0]));
+// // console.log(calcAge(years[1]));
+// // console.log(calcAge(years[3]));
+// // console.log(calcAge(years[2]));
+
+// // const ages = [
+// //   calcAge(years[0]),
+// //   calcAge(years[1]),
+// //   calcAge(years[2]),
+// //   calcAge(years[3]),
+// // ];
+
+// // console.log(ages);
+
+// //for loop
+
+// // const timeline = [1991, 2000, 2003, 1997];
+// // const aged = [];
+
+// // for (i = 0; i < timeline.length; i++) {
+// //   aged.push(2040 - timeline[i]);
+// // }
+
+// // console.log(aged);
+
+// //continue & Break
+
+// // const bag = [2020, 455, 303, "name", "apple"];
+
+// // for (i = 0; i < bag.length; i++) {
+// //   if (typeof bag[i] !== "string") continue;
+// //   console.log("HI");
+// // }
+
+// // for (i = 0; i < bag.length; i++) {
+// //   if (typeof bag[i] !== "number") break;
+// //   console.log("HI");
+// // }
+
+// // for (let i = bag.length - 1; 1 >= 0; i--) {
+// //   console.log(bag[i]);
+// // }
+
+// // for (let exercise = 1; exercise < 4; exercise++) {
+// //   console.log(`----Starting Exercise-----`);
+// //   for (let rep = 1; rep < 6; rep++) {
+// //     console.log(`Exercise ${exercise}:Lifting weights reptition ${rep}`);
+// //   }
+// // }
+
+// // const flights =
+// //   "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
+
+// // 🔴 Delayed Departure from FAO to TXL (11h25)
+// //              Arrival from BRU to FAO (11h45)
+// //   🔴 Delayed Arrival from HEL to FAO (12h05)
+// //            Departure from FAO to LIS (12h30)
+// // const getCode = (str) => str.slice(0, 3).toUpperCase();
+
+// // for (const flight of flights.split("+")) {
+// //   const [type, from, to, time] = flight.split(";");
+// //   const output = `${type.startsWith("_Delayed") ? "🔴" : ""}${type.replaceAll(
+// //     "_",
+// //     " "
+// //   )} ${getCode(from)} ${getCode(to)} (${time.replace(":", "h")})`.padStart(36);
+// //   console.log(output);
+// // }
+
+// // const flight = "LH340";
+// // const san = {
+// //   name: "Santhosh",
+// //   passport: 7788446657,
+// // };
+// // const checkIn = function (flightNum, passenger) {
+// //   flightNum = "LV778";
+// //   passenger.name = "Mr." + passenger.name;
+// //   if (passenger.passport === san.passport) {
+// //     alert("CheckedIn");
+// //   } else {
+// //     alert("Wrong entry");
+// //   }
+// // };
+// // checkIn(flight, san);
+// // console.log(flight);
+// // console.log(san);
+
+// // Functions using Callback fun
+
+// const oneWord = function (str) {
+//   return str.replace(/ /g, " ").toLowerCase();
 // };
-// const age2 = calAge2(2008);
-// console.log(age2);
-
-// Arrow function
-
-// const ageUntilRetierment = (birthYear, bestName) => {
-//   const age = 2037 - birthYear;
-//   const retiredAge = 60 - age;
-//   return `${bestName} retires at the age of ${retiredAge}`;
+// const upperFirstWord = function (str) {
+//   const [first, ...others] = str.split(" ");
+//   return [first.toUpperCase(), ...others].join(" ");
 // };
-// console.log(ageUntilRetierment(2004, "Anil"));
+// //Higher order fun
+// const transformer = function (str, fn) {
+//   console.log(`Origunal string :${str}`);
+//   console.log(`Transformed string :${fn(str)}`);
+//   console.log(`Transformed by :${fn.name}`);
+// };
+// transformer("javascript is best lang", upperFirstWord);
+// transformer("javascript is best lang", oneWord);
 
-// function calling another function
-// function cutPeices(fruit) {
-//   return fruit * 5;
-// }
+// //function return functions
+// const greet = function (greeting) {
+//   return function (name) {
+//     console.log(`${greeting} ${name}`);
+//   };
+// };
+// const greetingHey = greet("Hey");
+// greetingHey("Jonas");
 
-// function freshJuice(apple, orange) {
-//   const appleJuice = cutPeices(apple);
-//   const orangeJuice = cutPeices(orange);
-//   const juice = `juice with ${appleJuice} apple and ${orangeJuice} orange`;
-//   return juice;
-// }
-// const drinkJuices = freshJuice(4, 3);
-// console.log(drinkJuice);
-// Arrays exercise
+// const greetArr = (greeting) => (name) => console.log(`${greeting} ${name}`);
 
-// const calcAge = function (birthYear) {
-//   return 2037 - birthYear;
+// // Call & Apply method
+
+// const spiceJet = {
+//   airline: "spicejet",
+//   code: "SP998",
+//   bookings: [],
+//   book(flightNum, name) {
+//     console.log(
+//       `${name} booked a seat on ${this.airline} flight ${this.code} ${flightNum}`
+//     );
+//     this.bookings.push({ flight: `${this.code} ${flightNum}`, name });
+//   },
 // };
 
-// const years = [2008, 2015, 1998, 2000];
+// spiceJet.book(234, "Santhosh");
+// spiceJet.book(445, "janani");
 
-// console.log(calcAge(years[0]));
-// console.log(calcAge(years[1]));
-// console.log(calcAge(years[3]));
-// console.log(calcAge(years[2]));
-
-// const ages = [
-//   calcAge(years[0]),
-//   calcAge(years[1]),
-//   calcAge(years[2]),
-//   calcAge(years[3]),
-// ];
-
-// console.log(ages);
-
-//for loop
-
-// const timeline = [1991, 2000, 2003, 1997];
-// const aged = [];
-
-// for (i = 0; i < timeline.length; i++) {
-//   aged.push(2040 - timeline[i]);
-// }
-
-// console.log(aged);
-
-//continue & Break
-
-// const bag = [2020, 455, 303, "name", "apple"];
-
-// for (i = 0; i < bag.length; i++) {
-//   if (typeof bag[i] !== "string") continue;
-//   console.log("HI");
-// }
-
-// for (i = 0; i < bag.length; i++) {
-//   if (typeof bag[i] !== "number") break;
-//   console.log("HI");
-// }
-
-// for (let i = bag.length - 1; 1 >= 0; i--) {
-//   console.log(bag[i]);
-// }
-
-// for (let exercise = 1; exercise < 4; exercise++) {
-//   console.log(`----Starting Exercise-----`);
-//   for (let rep = 1; rep < 6; rep++) {
-//     console.log(`Exercise ${exercise}:Lifting weights reptition ${rep}`);
-//   }
-// }
-
-// const flights =
-//   "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
-
-// 🔴 Delayed Departure from FAO to TXL (11h25)
-//              Arrival from BRU to FAO (11h45)
-//   🔴 Delayed Arrival from HEL to FAO (12h05)
-//            Departure from FAO to LIS (12h30)
-// const getCode = (str) => str.slice(0, 3).toUpperCase();
-
-// for (const flight of flights.split("+")) {
-//   const [type, from, to, time] = flight.split(";");
-//   const output = `${type.startsWith("_Delayed") ? "🔴" : ""}${type.replaceAll(
-//     "_",
-//     " "
-//   )} ${getCode(from)} ${getCode(to)} (${time.replace(":", "h")})`.padStart(36);
-//   console.log(output);
-// }
-
-// const flight = "LH340";
-// const san = {
-//   name: "Santhosh",
-//   passport: 7788446657,
+// const spiceWings = {
+//   airline: "Flywing",
+//   code: "hF998",
+//   bookings: [],
 // };
-// const checkIn = function (flightNum, passenger) {
-//   flightNum = "LV778";
-//   passenger.name = "Mr." + passenger.name;
-//   if (passenger.passport === san.passport) {
-//     alert("CheckedIn");
-//   } else {
-//     alert("Wrong entry");
-//   }
+// const book = spiceJet.book;
+// book.call(spiceJet, 345, "Deena");
+// book.call(spiceWings, 787, "Bai");
+
+// //apply method
+// const flightbox = [346, "Mandela"];
+// book.apply(spiceWings, flightbox);
+// //bind method
+
+// const bookSW = book.bind(spiceJet);
+// bookSW(45, "Father");
+
+// const bookFD = book.bind(spiceWings, 46);
+// bookFD("manoj");
+
+// const addVAT = function (rate) {
+//   return function (value) {
+//     console.log(`Result:`, value + value * rate);
+//   };
 // };
-// checkIn(flight, san);
-// console.log(flight);
-// console.log(san);
+// const addTax = addVAT(0.23);
+// addTax(100);
 
-// Functions using Callback fun
+// //Closure
 
-const oneWord = function (str) {
-  return str.replace(/ /g, " ").toLowerCase();
-};
-const upperFirstWord = function (str) {
-  const [first, ...others] = str.split(" ");
-  return [first.toUpperCase(), ...others].join(" ");
-};
-//Higher order fun
-const transformer = function (str, fn) {
-  console.log(`Origunal string :${str}`);
-  console.log(`Transformed string :${fn(str)}`);
-  console.log(`Transformed by :${fn.name}`);
-};
-transformer("javascript is best lang", upperFirstWord);
-transformer("javascript is best lang", oneWord);
+// let f;
 
-//function return functions
-const greet = function (greeting) {
-  return function (name) {
-    console.log(`${greeting} ${name}`);
-  };
-};
-const greetingHey = greet("Hey");
-greetingHey("Jonas");
+// const g = function () {
+//   const a = 23;
+//   f = function () {
+//     console.log(a * 2);
+//   };
+// };
 
-const greetArr = (greeting) => (name) => console.log(`${greeting} ${name}`);
+// const h = function () {
+//   const b = 555;
+//   f = function () {
+//     console.log(b * 2);
+//   };
+// };
 
-// Call & Apply method
+// g();
+// f();
 
-const spiceJet = {
-  airline: "spicejet",
-  code: "SP998",
-  bookings: [],
-  book(flightNum, name) {
-    console.log(
-      `${name} booked a seat on ${this.airline} flight ${this.code} ${flightNum}`
-    );
-    this.bookings.push({ flight: `${this.code} ${flightNum}`, name });
-  },
-};
+// // Reassigning f function
 
-spiceJet.book(234, "Santhosh");
-spiceJet.book(445, "janani");
-
-const spiceWings = {
-  airline: "Flywing",
-  code: "hF998",
-  bookings: [],
-};
-const book = spiceJet.book;
-book.call(spiceJet, 345, "Deena");
-book.call(spiceWings, 787, "Bai");
-
-//apply method
-const flightbox = [346, "Mandela"];
-book.apply(spiceWings, flightbox);
-//bind method
-
-const bookSW = book.bind(spiceJet);
-bookSW(45, "Father");
-
-const bookFD = book.bind(spiceWings, 46);
-bookFD("manoj");
-
-const addVAT = function (rate) {
-  return function (value) {
-    console.log(`Result:`, value + value * rate);
-  };
-};
-const addTax = addVAT(0.23);
-addTax(100);
-
-//Closure
-
-let f;
-
-const g = function () {
-  const a = 23;
-  f = function () {
-    console.log(a * 2);
-  };
-};
-
-const h = function () {
-  const b = 555;
-  f = function () {
-    console.log(b * 2);
-  };
-};
-
-g();
-f();
-
-// Reassigning f function
-
-h();
-f();
+// h();
+// f();
