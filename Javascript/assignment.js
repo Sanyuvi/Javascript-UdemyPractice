c856774ae4a8535165cdb148c8143bbd4e9d8f58
Sanyuvi <santhoshh2197@gmail.com>
2024-02-05 23:57:35 +0530

    new code challenge added

--- Javascript/assignment.js
+++ Javascript/assignment.js
@@ -351,3 +351,20 @@ for (const [t, e] of gameEvents) {
   t <= 45 && console.log(`[First Half] ${t}:${e}`);
   t >= 45 && console.log(`[Second Half] ${t}:${e}`);
 }
+
+////////////
+const checkDogs = function (dogsJulia, dogsKate) {
+  const dogsJuliaCorrected = dogsJulia.slice();
+  dogsJuliaCorrected.splice(0, 1);
+  dogsJuliaCorrected.splice(-2);
+  const dog = dogsJuliaCorrected.concat(dogsKate);
+  console.log(dog);
+  dog.forEach(function (dog, i) {
+    if (dog >= 3) {
+      console.log(`Dog number ${i + 1} is an adult, and is ${dog} years old`);
+    } else {
+      console.log(`Dog number ${i + 1} is still a puppy`);
+    }
+  });
+};
+checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
